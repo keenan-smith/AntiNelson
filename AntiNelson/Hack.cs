99bd4ebfea91b28ddda5479f41266c7ce4704bdd
Kunii <robin.t-egede@hotmail.com>
2016-08-04 01:33:58 +0200

    Initial shait

---
+++ AntiNelson/Hack.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+namespace AntiNelson
+{
+    public class Hack : MonoBehaviour
+    {
+
+        public static GameObject hookObj = null;
+        public static Camera cam = null;
+
+        public static bool showGUI = true;
+
+        public static void reHook()
+        {
+
+            try
+            {
+                while (true)
+                {
+
+                    if (EntryPoint.instance == null || hookObj == null)
+                    {
+
+                        hookObj = new GameObject();
+                        EntryPoint.instance = hookObj.AddComponent<EntryPoint>();
+                        DontDestroyOnLoad(hookObj);
+
+                    }
+
+                    if (!EntryPoint.waitForRelaunch)
+                        Thread.Sleep(5000);
+
+                    EntryPoint.waitForRelaunch = false;
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                Debug.LogWarning("============ HACK CRASHED START ============");
+                Debug.LogException(e);
+                Debug.LogWarning("============ HACK CRASHED END ============");
+
+            }
+
+        }
+
+        public void _Update()
+        {
+
+            if (isKeyDown(KeyCode.F1))
+                showGUI = !showGUI;
+
+        }
+
+        public void _OnGUI()
+        {
+
+            if (showGUI)
+            {
+
+                GUI.color = Color.white;
+
+                GUILayout.BeginArea(new Rect(Screen.width - 170F, 10F, 160F, Screen.height));
+                GUILayout.BeginVertical();
+
+                GUILayout.Label("Anti Nelson");
+
+                if (Camera.main != null)
+                    if (cam != null)
+                    {
+                        Vector3 _cam = cam.transform.position;
+                        GUILayout.Label("Position: " + (int)_cam.x + ", " + (int)_cam.y + ", " + (int)_cam.z);
+                    }
+                    else cam = Camera.main;
+
+                GUILayout.EndVertical();
+                GUILayout.EndArea();
+
+            }
+
+        }
+
+        public bool isKeyDown(KeyCode key)
+        {
+
+            return key != KeyCode.None && Input.GetKeyDown(key);
+
+        }
+
+    }
+}
---
+++ AntiNelson/Loading/EntryPoint.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading;
+using UnityEngine;
+
+namespace AntiNelson
+{
+    public class EntryPoint : MonoBehaviour
+    {
+
+        public static EntryPoint instance = null;
+        public static Hack hackInstance = null;
+
+        public static bool waitForRelaunch = false;
+        public static bool hackCrashed = false;
+
+        public static Thread runThread = null;
+
+        public static void Launch()
+        {
+
+            runThread = new Thread(Hack.reHook);
+            runThread.Start();
+
+        }
+
+        public static void killAndRelaunch()
+        {
+
+            DestroyImmediate(Hack.hookObj);
+            runThread.Abort();
+            hackInstance = new Hack();
+            instance = null;
+
+            Launch();
+
+        }
+
+        public void Update()
+        {
+
+            if (!waitForRelaunch && !hackCrashed)
+                try
+                {
+                    hackInstance._Update();
+                }
+                catch (Exception e)
+                {
+
+                    Debug.LogException(e);
+
+                    if (!e.ToString().ToLower().Contains("repaint"))
+                        hackCrashed = true;
+
+                }
+
+        }
+
+        public void OnGUI()
+        {
+
+            if (!waitForRelaunch && !hackCrashed)
+                try
+                {
+                    hackInstance._OnGUI();
+                }
+                catch (Exception e)
+                {
+
+                    Debug.LogException(e);
+
+                    if (!e.ToString().ToLower().Contains("repaint"))
+                        hackCrashed = true;
+
+                }
+
+            if (hackCrashed)
+            {
+
+                GUILayout.BeginArea(new Rect(10F, 10F, 200F, Screen.height));
+                GUILayout.BeginVertical();
+
+                int size = GUI.skin.label.fontSize;
+                float height = GUI.skin.label.fixedHeight;
+                GUI.skin.label.fontSize += 25;
+                GUI.skin.label.fixedHeight = 50;
+                GUI.color = Color.red;
+
+                GUILayout.Label("HACK CRASHED");
+
+                GUI.skin.label.fontSize = size;
+                GUI.skin.label.fixedHeight = height;
+                GUI.color = Color.white;
+
+                if (GUILayout.Button("Relaunch"))
+                    killAndRelaunch();
+
+                GUILayout.EndVertical();
+                GUILayout.EndArea();
+
+            }
+
+        }
+
+    }
+}
