012ceac2271731a76339e18e27ae75b8a6e0f901
AtiLion <eliteinfinity1337@gmail.com>
2016-09-01 10:47:16 +0200

    Added Player

---
+++ AntiNelson/API/Extensions/PlayerProfile.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using SDG.Unturned;
+using PointBlank.API.Enumerables;
+
+namespace PointBlank.API.Extensions
+{
+    public class PlayerProfile
+    {
+        #region Variables
+        private bool _isVisible;
+        private bool _vacBanned;
+        private bool _tradeBanned;
+        private bool _isLimited;
+        private int _privacyState; // 0 = Public, 1 = Friends Only, 2 = Private
+        private string _link;
+        #endregion
+
+        #region Propertys
+        public bool isVisible
+        {
+            get
+            {
+                return _isVisible;
+            }
+        }
+
+        public bool isVacBanned
+        {
+            get
+            {
+                return _vacBanned;
+            }
+        }
+
+        public bool isTradeBanned
+        {
+            get
+            {
+                return _tradeBanned;
+            }
+        }
+
+        public bool isLimited
+        {
+            get
+            {
+                return _isLimited;
+            }
+        }
+
+        public string link
+        {
+            get
+            {
+                return _link;
+            }
+        }
+
+        public PrivacyState privacyState
+        {
+            get
+            {
+                if (_privacyState == 0)
+                    return PrivacyState.PUBLIC;
+                else if (_privacyState == 1)
+                    return PrivacyState.FRIENDS_ONLY;
+                else if (_privacyState == 2)
+                    return PrivacyState.PRIVATE;
+                else
+                    return PrivacyState.NONE;
+            }
+        }
+        #endregion
+
+        public PlayerProfile(string url)
+        {
+            _link = url;
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(url);
+            XmlElement root = doc.DocumentElement;
+            _isVisible = (root.SelectSingleNode("visibilityState").InnerText == "1");
+            _vacBanned = (root.SelectSingleNode("vacBanned").InnerText == "1");
+            _tradeBanned = (root.SelectSingleNode("tradeBanState").InnerText != "None");
+            _isLimited = (root.SelectSingleNode("isLimitedAccount").InnerText == "1");
+            string privacy = root.SelectSingleNode("privacyState").InnerText;
+            if (privacy == "public")
+                _privacyState = 0;
+            else if (privacy == "friendsonly")
+                _privacyState = 1;
+            else if (privacy == "private")
+                _privacyState = 2;
+        }
+    }
+}
---
+++ AntiNelson/API/PointPlayer.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using SDG.Unturned;
+using Steamworks;
+using PointBlank.API.Extensions;
+
+namespace PointBlank.API
+{
+    public class PointPlayer : MonoBehaviour
+    {
+
+        #region RandomVars
+        private byte[] prevHash = null;
+        private bool testingAntiSpy = false;
+        #endregion
+
+        #region Variables
+        private SteamPlayer _steamPlayer;
+        private uint _IP;
+        private PlayerProfile _profile;
+        #endregion
+
+        #region Propertys
+        public SteamPlayer steamPlayer
+        {
+            get
+            {
+                return _steamPlayer;
+            }
+        }
+
+        public Player player
+        {
+            get
+            {
+                return _steamPlayer.player;
+            }
+        }
+
+        public SteamPlayerID playerID
+        {
+            get
+            {
+                return _steamPlayer.playerID;
+            }
+        }
+
+        public CSteamID steamID
+        {
+            get
+            {
+                return _steamPlayer.playerID.steamID;
+            }
+        }
+
+        public ulong steam64
+        {
+            get
+            {
+                return _steamPlayer.playerID.steamID.m_SteamID;
+            }
+        }
+
+        public uint IP
+        {
+            get
+            {
+                return _IP;
+            }
+        }
+
+        public PlayerProfile steamProfile
+        {
+            get
+            {
+                return _profile;
+            }
+        }
+        #endregion
+
+        public PointPlayer(SteamPlayer p)
+        {
+            _steamPlayer = p;
+
+            P2PSessionState_t sState;
+            if (SteamGameServerNetworking.GetP2PSessionState(steamID, out sState))
+                _IP = sState.m_nRemoteIP;
+            else
+                _IP = 0u;
+
+            _profile = new PlayerProfile("http://steamcommunity.com/profiles/" + steam64 + "/?xml=1");
+        }
+
+        #region Functions
+
+        public bool ban(string reason, uint duration, bool IPBan, CSteamID judge)
+        {
+            try
+            {
+                SteamBlacklist.ban(
+                    steamID,
+                    (IPBan ? IP : 0u),
+                    judge,
+                    reason,
+                    duration
+                );
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                Console.WriteLine("ERROR: Exception while attempting to ban player!");
+                return false;
+            }
+        }
+
+        public bool kick(string reason)
+        {
+            try
+            {
+                Provider.kick(steamID, reason);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                Console.WriteLine("ERROR: Exception while attempting to kick player!");
+                return false;
+            }
+        }
+
+        public bool openURL(string url, string message)
+        {
+            try
+            {
+                player.sendBrowserRequest(message, url);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                Console.WriteLine("ERROR: Exception while attempting to open url for player!");
+                return false;
+            }
+        }
+
+        public bool testAntiSpy()
+        {
+            try
+            {
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                Console.WriteLine("ERROR: Exception while attempting to test player for antispy!");
+                return false;
+            }
+        }
+        #endregion
+
+        #region Events
+        private void onSpyReady(object sender, PlayerSpyReady args)
+        {
+            if (testingAntiSpy)
+            {
+                if (prevHash == null)
+                {
+                    
+                }
+            }
+        }
+        #endregion
+    }
+}
