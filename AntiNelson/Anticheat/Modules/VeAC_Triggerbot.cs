613cdafa1d6c68b02a1398e7a331ecee37b10a40
AtiLion <eliteinfinity1337@gmail.com>
2016-10-27 11:59:29 +0200

    I am bored

--- AntiNelson/Anticheat/Modules/VeAC_Triggerbot.cs
+++ AntiNelson/Anticheat/Modules/VeAC_Triggerbot.cs
@@ -5,13 +5,25 @@ using System.Text;
 using UnityEngine;
 using SDG.Unturned;
 using PointBlank.Anticheat.Extensions;
+using PointBlank.API.Server;
 
 namespace PointBlank.Anticheat.Modules
 {
     internal class VeAC_Triggerbot : VeAC_Module
     {
+        #region Variables
+        private Dictionary<PBPlayer, float> _shootMemory = new Dictionary<PBPlayer, float>();
+        #endregion
+
         #region Mono Functions
         public void Start()
+        {
+            PBPlayer.OnPlayerMove += new PBPlayer.PlayerMoveHandler(Event_PlayerMove);
+        }
+        #endregion
+
+        #region Event Functions
+        private void Event_PlayerMove(PBPlayer player, Vector3 position, Quaternion rotation)
         {
 
         }
