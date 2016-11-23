6cc256fcea2c1b6154006771009a32328522dee4
AtiLion <eliteinfinity1337@gmail.com>
2016-11-23 12:01:11 +0100

    Started Working On Weapon Hacks

---
+++ ManPAD/ManPAD_API/Types/WAssetSave.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using SDG.Unturned;
+
+namespace ManPAD.ManPAD_API.Types
+{
+    public class WAssetSave
+    {
+        public ItemWeaponAsset asset;
+
+        public float recoilMax_x;
+        public float recoilMax_y;
+        public float recoilMin_x;
+        public float recoilMin_y;
+
+        public float spreadAim;
+        public float spreadHip;
+
+        public float shakeMax_x;
+        public float shakeMax_y;
+        public float shakeMax_z;
+        public float shakeMin_x;
+        public float shakeMin_y;
+        public float shakeMin_z;
+
+        public float range;
+
+        public WAssetSave(ItemGunAsset asset)
+        {
+            this.asset = asset;
+
+            recoilMax_x = asset.recoilMax_x;
+            recoilMax_y = asset.recoilMax_y;
+            recoilMin_x = asset.recoilMin_x;
+            recoilMin_y = asset.recoilMin_y;
+
+            spreadAim = asset.spreadAim;
+            spreadHip = asset.spreadHip;
+
+            shakeMax_x = asset.shakeMax_x;
+            shakeMax_y = asset.shakeMax_y;
+            shakeMax_z = asset.shakeMax_z;
+            shakeMin_x = asset.shakeMin_x;
+            shakeMin_y = asset.shakeMin_y;
+            shakeMin_z = asset.shakeMin_z;
+
+            range = asset.range;
+        }
+
+        public WAssetSave(ItemMeleeAsset asset)
+        {
+            range = asset.range;
+        }
+    }
+}
---
+++ ManPAD/ManPAD_Hacks/MainMenu/MP_Weapons.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ManPAD.ManPAD_API.Types;
+using ManPAD.ManPAD_API.GUI.Attributes;
+using ManPAD.ManPAD_API.GUI.Extensions;
+using SDG.Unturned;
+using UnityEngine;
+
+namespace ManPAD.ManPAD_Hacks.MainMenu
+{
+    [MenuOption(7, "Weapons", 300f, 300f)]
+    public class MP_Weapons : MenuOption
+    {
+        #region Variables
+        private Dictionary<byte[], WAssetSave> _backup = new Dictionary<byte[], WAssetSave>();
+
+        public static bool norecoil = true;
+        public static bool nospread = true;
+        public static bool noshake = true;
+        public static bool nosway = true;
+        public static bool attackThroughWalls = false;
+        public static bool autoreload = false;
+        public static bool farReach = false;
+        #endregion
+
+        #region Mono Functions
+        public void Update()
+        {
+            if (!Variables.isInGame)
+                return;
+
+            if (Player.player.equipment.isSelected && Player.player.equipment.asset != null && Player.player.equipment.useable != null && Player.player.equipment.asset is ItemWeaponAsset)
+            {
+                byte[] hash = Player.player.equipment.asset.hash;
+                if ((norecoil || nospread || noshake || farReach) && !_backup.ContainsKey(hash))
+                {
+                    if (Player.player.equipment.asset is ItemGunAsset)
+                        _backup.Add(hash, new WAssetSave((ItemGunAsset)Player.player.equipment.asset));
+                    else
+                        _backup.Add(hash, new WAssetSave((ItemMeleeAsset)Player.player.equipment.asset));
+                }
+
+                if (Player.player.equipment.asset is ItemGunAsset)
+                {
+                    if (norecoil)
+                        setRecoil((ItemGunAsset)Player.player.equipment.asset, 0f, 0f, 0f, 0f);
+                    else
+                        setRecoil((ItemGunAsset)Player.player.equipment.asset, _backup[hash].recoilMax_x, _backup[hash].recoilMax_y, _backup[hash].recoilMin_x, _backup[hash].recoilMin_y);
+
+                    if (nospread && !Variables.isSpying)
+                        setSpread((ItemGunAsset)Player.player.equipment.asset, 0f, 0f);
+                    else
+                        setSpread((ItemGunAsset)Player.player.equipment.asset, _backup[hash].spreadAim, _backup[hash].spreadHip);
+
+                    if (noshake)
+                        setShake((ItemGunAsset)Player.player.equipment.asset, 0f, 0f, 0f, 0f, 0f, 0f);
+                    else
+                        setShake((ItemGunAsset)Player.player.equipment.asset, _backup[hash].shakeMax_x, _backup[hash].shakeMax_y, _backup[hash].shakeMax_z, _backup[hash].shakeMin_x, _backup[hash].shakeMin_y, _backup[hash].shakeMin_z);
+                }
+
+                if (Player.player.equipment.useable is UseableGun)
+                {
+                    if (nosway)
+                        setSway((UseableGun)Player.player.equipment.useable, 4u);
+
+                    typeof(UseableGun).GetMethod("updateCrosshair", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(Player.player.equipment.useable, new object[0]);
+                }
+
+                if (Player.player.equipment.asset is ItemMeleeAsset)
+                {
+                    if (farReach)
+                        setRange((ItemWeaponAsset)Player.player.equipment.asset, 26f);
+                    else
+                        setRange((ItemWeaponAsset)Player.player.equipment.asset, _backup[hash].range);
+                }
+            }
+        }
+        #endregion
+
+        #region Functions
+        public override void runGUI()
+        {
+            norecoil = GUILayout.Toggle(norecoil, "No Recoil");
+            noshake = GUILayout.Toggle(noshake, "No Shake");
+            nospread = GUILayout.Toggle(nospread, "No Spread");
+            nosway = GUILayout.Toggle(nosway, "No Sway");
+            farReach = GUILayout.Toggle(farReach, "Far Reach");
+            autoreload = GUILayout.Toggle(autoreload, "Auto Reload");
+            attackThroughWalls = GUILayout.Toggle(attackThroughWalls, "Attack Through Walls");
+        }
+
+        private void setRecoil(ItemGunAsset asset, float maxX, float maxY, float minX, float minY)
+        {
+            typeof(ItemGunAsset).GetField("_recoilMax_x", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, maxX);
+            typeof(ItemGunAsset).GetField("_recoilMax_y", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, maxY);
+            typeof(ItemGunAsset).GetField("_recoilMin_x", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, minX);
+            typeof(ItemGunAsset).GetField("_recoilMin_y", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, minY);
+        }
+
+        private void setSpread(ItemGunAsset asset, float aim, float hip)
+        {
+            typeof(ItemGunAsset).GetField("_spreadAim", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, aim);
+            typeof(ItemGunAsset).GetField("_spreadHip", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, hip);
+        }
+
+        private void setShake(ItemGunAsset asset, float maxX, float maxY, float maxZ, float minX, float minY, float minZ)
+        {
+            typeof(ItemGunAsset).GetField("_shakeMax_x", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, maxX);
+            typeof(ItemGunAsset).GetField("_shakeMax_y", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, maxY);
+            typeof(ItemGunAsset).GetField("_shakeMax_z", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, maxZ);
+            typeof(ItemGunAsset).GetField("_shakeMin_x", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, minX);
+            typeof(ItemGunAsset).GetField("_shakeMin_y", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, minY);
+            typeof(ItemGunAsset).GetField("_shakeMin_z", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, minZ);
+        }
+
+        private void setSway(UseableGun asset, uint steady)
+        {
+            typeof(UseableGun).GetField("steadyAccuracy", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, steady);
+        }
+
+        private void setRange(ItemWeaponAsset asset, float range)
+        {
+            typeof(ItemWeaponAsset).GetField("_range", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(asset, range);
+        }
+        #endregion
+    }
+}
