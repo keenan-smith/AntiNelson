694914e152d0dda99f807b487e80f037cfe005cb
Kunii <robin.t-egede@hotmail.com>
2016-12-18 19:21:05 +0100

    Updated monoloader

--- Monoloader/Main.cpp
+++ Monoloader/Main.cpp
@@ -10,6 +10,21 @@ void __fastcall eject(DWORD exit)
 
 }
 
+std::vector<char> ReadAllBytes(char const* filename)
+{
+
+	std::ifstream ifs(filename, std::ios::binary | std::ios::ate);
+	std::ifstream::pos_type pos = ifs.tellg();
+
+	std::vector<char>  result(pos);
+
+	ifs.seekg(0, std::ios::beg);
+	ifs.read(&result[0], pos);
+
+	return result;
+
+}
+
 void __fastcall inject(char* path, char* _namespace, char* _class, char* method)
 {
 
@@ -24,10 +39,7 @@ void __fastcall inject(char* path, char* _namespace, char* _class, char* method)
 	}
 
 	monoAttachToThread(rootDomain);
-	printf("Attached to mono main thread\n");
-
 	setMonoSecurity(MONO_SECURITY_MODE_NONE);
-	printf("Set mono security to MONO_SECURITY_MODE_NONE\n");
 
 	int data = openAssembly(getMonoDomain(), path);
 	PVOID pMethod = getClassMethodFromName(getClassFromName(getAssemblyImageName(data), _namespace, _class), method, 0);
@@ -41,7 +53,9 @@ void __fastcall inject(char* path, char* _namespace, char* _class, char* method)
 	}
 
 	printf("Payload: %p\n", pMethod);
-	printf("Payload delivered: %d", invokeRuntime(pMethod, NULL, NULL, NULL));
+	printf("Payload delivered: %d\n", invokeRuntime(pMethod, NULL, NULL, NULL));
+
+	loadBE();
 
 	eject(0);
 
@@ -52,22 +66,39 @@ DWORD WINAPI initInject(LPVOID param)
 
 	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
 
+	AllocConsole();
+	freopen("CONOUT$", "w", stdout);
+
+	HMODULE ASM = NULL;
+
+	while (!ASM)
+	{
+
+		Sleep(250);
+		ASM = GetModuleHandle("CSteamworks.dll");
+
+	}
+
 	HMODULE mono = NULL;
 
 	while (!mono)
 	{
 
-		mono = GetModuleHandle("mono.dll");
 		Sleep(250);
+		mono = GetModuleHandle("mono.dll");
 
 	}
 
-	char tempPath[MAX_PATH * 4];
-	GetTempPathA(MAX_PATH * 4, tempPath);
-	sprintf_s(tempPath, "%s.-", tempPath);
-	printf("%s\n", tempPath);
+	MonoInit(mono);
 
-	inject(tempPath, "MonolandLoader", "ManPad", "MeLikeyBigBoom");
+	printf("%i\n", mono);
+	printf("%d\n", GetCurrentProcess());
+
+	char tPath[MAX_PATH * 4];
+	_getcwd(tPath, sizeof tPath);
+	sprintf(tPath, "%s\\MLoader.dll", tPath);
+
+	inject(tPath, "MLoader", "Loading", "executeLoad");
 
 	return 1;
 
--- Monoloader/Main.h
+++ Monoloader/Main.h
@@ -1,13 +1,14 @@
+#pragma once
+#ifndef _MONO_FUNCTIONS_
+#define _MONO_FUNCTIONS_
+
 #include <fstream>
 #include <vector>
 #include <wchar.h>
 #include <Windows.h>
 #include <stdint.h>
 #include <stdio.h>
-
-#pragma once
-#ifndef _MONO_FUNCTIONS_
-#define _MONO_FUNCTIONS_
+#include <direct.h>
 
 typedef enum {
 	MONO_SECURITY_MODE_NONE,
@@ -26,6 +27,8 @@ typedef PVOID(*mono_class_from_name_t) (int a_image, const char* a_name_space, c
 typedef PVOID(*mono_class_get_method_from_name_t) (PVOID a_klass, const char *a_name, int a_param_count);
 typedef int(*mono_runtime_invoke_t) (PVOID a_method, void *a_obj, void **a_params, int **a_exc);
 typedef PVOID(*mono_thread_attach_t) (int a_domain);
+typedef int(__cdecl* mono_image_open_from_data_full) (int data, unsigned int data_len, int need_copy, int *status, int refonly);
+typedef int(__cdecl* mono_assembly_load_from_full) (int image, int *fname, int *status, bool refonly);
 
 mono_security_set_t setMonoSecurity;
 mono_domain_get_t getMonoDomain;
@@ -34,8 +37,10 @@ mono_domain_assembly_open_t openAssembly;
 mono_assembly_get_image_t getAssemblyImageName;
 mono_class_from_name_t getClassFromName;
 mono_class_get_method_from_name_t getClassMethodFromName;
-mono_runtime_invoke_t invokeRuntime;;
+mono_runtime_invoke_t invokeRuntime;
 mono_thread_attach_t monoAttachToThread;
+mono_image_open_from_data_full mono_image_open_from_data_full_;
+mono_assembly_load_from_full mono_assembly_load_from_full_;
 
 void __fastcall MonoInit(HMODULE hMono)
 {
@@ -49,6 +54,20 @@ void __fastcall MonoInit(HMODULE hMono)
 	getClassMethodFromName = (mono_class_get_method_from_name_t)GetProcAddress(hMono, "mono_class_get_method_from_name");
 	invokeRuntime = (mono_runtime_invoke_t)GetProcAddress(hMono, "mono_runtime_invoke");
 	monoAttachToThread = (mono_thread_attach_t)GetProcAddress(hMono, "mono_thread_attach");
+	mono_image_open_from_data_full_ = (mono_image_open_from_data_full)GetProcAddress(hMono, "mono_image_open_from_data");
+	mono_assembly_load_from_full_ = (mono_assembly_load_from_full)GetProcAddress(hMono, "mono_assembly_load_from_full");
+
+}
+
+void __fastcall loadBE()
+{
+
+	char tPath[MAX_PATH * 4];
+	_getcwd(tPath, sizeof tPath);
+	sprintf(tPath, "%s\\BattlEye\\BEClient_x64.dll", tPath);
+
+	printf("BATTLEYE: %X", LoadLibraryA(tPath));
+
 
 }
 
