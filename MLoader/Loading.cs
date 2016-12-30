76bf2205de76f8379f0cbc0c35adec42d403d0f8
Keenan Smith <zoomy500@gmail.com>
2016-12-30 14:13:45 -0500

    fixed MLoader

--- MLoader/Loading.cs
+++ MLoader/Loading.cs
@@ -11,31 +11,12 @@ namespace MLoader
 {
     public class Loading
     {
-        private static bool injected = false;
-        private static Thread _thread;
-
         public static void executeLoad()
         {
-            _thread = new Thread(new ThreadStart(runThread));
-            _thread.Start();
-        }
+            byte[] ba = File.ReadAllBytes(Directory.GetCurrentDirectory() + "\\ManPAD.dll");
 
-        private static void runThread()
-        {
-            while (!injected)
-            {
-                Thread.Sleep(200);
-                try
-                {
-                    Assembly asm = Assembly.LoadFrom(Directory.GetCurrentDirectory() + "/ManPAD.dll");
-                    asm.GetTypes().Where(a => a.IsClass && a.Name == "Hook").First().GetMethod("callMeToHook", BindingFlags.Static | BindingFlags.Public).Invoke(null, new object[0]);
-                    injected = true;
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.ToString());
-                }
-            }
+            Type t = Assembly.Load(ba).GetType("ManPAD.ManPAD_Loading.Hook");
+            t.GetMethod("callMeToHook").Invoke(Activator.CreateInstance(t), null);
         }
     }
 }
